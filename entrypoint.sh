5a8aef714c30fe05998fd904a18e8ecda9225956
Denys Chmeliuk <denys.chmeliuk@jellyfish.com>
2024-03-28 17:05:07 +0100

    vendor

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,12 @@ fi
 
 if [ ! -z "$REQUIREMENTS_FILE" ]
 then
-      pip install -r "$REQUIREMENTS_FILE" -t ./vendor
+      vendor_part=""
+      if [! -z "$INSTALL_VENDOR" ]
+      then
+          vendor_part=" -t ./vendor"
+      fi
+      pip install -r "$REQUIREMENTS_FILE" $vendor_part
 fi
 
 if ! [ -z "$PROJECT_DIR" ]
